64d0bf5414df79bb791721cc4de6126ffc90ebfa
Kaike Araújo <kaikeuniespfds@gmail.com>
2026-04-07 12:41:38 -0300

    Add files via upload

---
+++ src/main/java/br/com/alunoonline/api/model/Professor.java
@@ -0,0 +1,23 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "professor")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+
+
+}
